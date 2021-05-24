f275855893762902d29d829c36015dcba3ad6c79
sivchari <shibuuuu5@gmail.com>
2021-05-24 20:22:57 +0900

    remove .idea and fix test name

--- client_test.go
+++ client_test.go
@@ -979,7 +979,7 @@ func TestCreatePage(t *testing.T) {
 			expError: nil,
 		},
 		{
-			name: "valid case: ParentType is ParentTypeDatabase and successful response",
+			name: "database parent, successful response",
 			params: notion.CreatePageParams{
 				ParentType: notion.ParentTypeDatabase,
 				ParentID:   "b0668f48-8d66-4733-9bdb-2f82215707f7",
